c2a423d92e6524394a1c457b109b8034f9e05efc
AmbiaaH <ambiaahussain@gmail.com>
2025-04-09 08:29:13 +0100

    change ip address

--- src/main/java/client/GrpcClient.java
+++ src/main/java/client/GrpcClient.java
@@ -8,7 +8,7 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 public class GrpcClient {
     public static void main(String[] args) {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("34.105.156.173", 8080)
           .usePlaintext()
           .build();
 
